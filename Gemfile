cd20cf9d2024c20b620bb1bb34055c9217927453
mcoenca <m.coenca@gmail.com>
2014-02-05 22:26:12 +0100

    Initial commit before databases

---
+++ Gemfile
@@ -0,0 +1,25 @@
+source 'https://rubygems.org'
+ruby '2.0.0'
+#ruby-gemset=railstutorial_rails_4_0
+
+gem 'rails', '4.0.2'
+
+group :development do
+  gem 'mysql2'
+end
+
+gem 'sass-rails', '4.0.1'
+gem 'uglifier', '2.1.1'
+gem 'coffee-rails', '4.0.1'
+gem 'jquery-rails', '3.0.4'
+gem 'turbolinks', '1.1.1'
+gem 'jbuilder', '1.0.2'
+
+group :doc do
+  gem 'sdoc', '0.3.20', require: false
+end
+
+group :production do
+  gem 'mysql2'
+  gem 'rails_12factor', '0.0.2'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SmokitBack::Application.config.secret_key_base = '4a3292881f2d7af0d5b325eea62d93ea8ca347a27952fb858a82e218124898c6225a0acab897564937e42198bb7ef81eca1e21133a6be64a3dfea829640b98b5'
